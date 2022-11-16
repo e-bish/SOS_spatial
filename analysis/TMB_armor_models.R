f36de04f137ad9b94da23f7706f6bcf5f7efa270
Emily Bishop <ebish@uw.edu>
2022-11-16 12:03:48 -0800

    add some tmb file examples to play around with

---
+++ analysis/TMB_armor_models.R
@@ -0,0 +1,38 @@
+require(TMB)
+
+### First run - no random effects at all ####
+
+DATA_VECTOR(y);
+DATA_VECTOR(x);
+
+
+data <- list(n = ndata,
+             nsites = nsites,
+             X = as.matrix(X),
+             y = thedata$y,
+             Z = as.matrix(Z)
+)
+params <- list(B = matrix(0, nrow = nsites, ncol = 2),
+               bbar = 0,
+               ubar = 0,
+               logsigma =0,
+               logsigma_re = c(0,0)
+)
+
+
+compile("tmb/linreg.cpp")
+dyn.load(dynlib("tmb/linreg"))
+
+model <-
+  MakeADFun(
+    data = data,
+    parameters = parameters,
+    DLL = "linreg")
+#,
+#   random = "B"
+#  )
+fit <- nlminb(model$par, model$fn, model$gr)
+
+
+library(TMB)
+runExample(all=TRUE)
--- tmb/linreg.cpp
+++ tmb/linreg.cpp
@@ -1,28 +1,14 @@
+// Simple linear regression.
 #include <TMB.hpp>
-
 template<class Type>
 Type objective_function<Type>::operator() ()
-  {
-  DATA_VECTOR(y);
+{
+  DATA_VECTOR(Y);
   DATA_VECTOR(x);
-  int n = y.size();
-  
-  PARAMETER(b0);
-  PARAMETER(b1);
-  PARAMETER(logsigma);
-  // Create objectes
-  vector<Type> yfit(n);
-  Type sigma;
-  sigma = exp(logsigma);
-  Type neglogL = 0.0;
-  
-  for(int i = 0; i < n; i++) { //indexing starts at zero, i through n
-    yfit(i) = b0 + b1 * x(i);
-    neglogL += -dnorm(y(i), yfit(i), sigma, true);
-    
-  }
-  
-  
-  return neglogL;
-  }
-
+  PARAMETER(a);
+  PARAMETER(b);
+  PARAMETER(logSigma);
+  ADREPORT(exp(2*logSigma));
+  Type nll = -sum(dnorm(Y, a+b*x, exp(logSigma), true));
+  return nll;
+}
