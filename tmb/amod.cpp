cbb5b523474a924f36d9fa36be12df68e259486a
Emily Bishop <ebish@uw.edu>
2022-11-22 13:58:22 -0800

    attempt to do proper neg binomial

--- tmb/amod.cpp
+++ tmb/amod.cpp
@@ -3,14 +3,277 @@
 template<class Type>
 Type objective_function<Type>::operator() ()
 {
-  DATA_VECTOR(total);
+  //specify data
+  DATA_VECTOR(y);
   DATA_VECTOR(logyday);
   DATA_VECTOR(X100m);
+  int n = y.size();
+  
+  //specify parameters
   PARAMETER(b0);
   PARAMETER(b1);
   PARAMETER(b2);
-  PARAMETER(logSigma);
-  ADREPORT(exp(2*logSigma));
-  Type nll = -sum(dnbinom(total, b0 + b1*logyday + b2*X100m, exp(logSigma), true));
-  return nll;
+  //PARAMETER(log_mu); 
+  PARAMETER(log_var);
+
+  //create objects
+  vector<Type> yfit(n); 
+  //Type mu=exp(log_mu);
+  Type var=exp(log_var);
+  Type neglogL = 0.0; //initial starting value for neg log likelihood
+
+  //negative log likelihood
+  for(int i = 0; i < n; i++) { 
+    yfit(i) = b0 + b1 * logyday(i) + b2 * X100m(i);
+    neglogL += -dnbinom2(y(i), yfit(i), var, true);
+  }
+  
+  //stuff to report 
+  return neglogL;
+  ADREPORT(var);
+  //ADREPORT(mu);
+  //ADREPORT(exp(2*logSigma));  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
