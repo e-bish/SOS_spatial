a7b2d58936af440a940270626a67432e59331905
Emily Bishop <ebish@uw.edu>
2022-11-22 14:06:04 -0800

    update neg binom

--- analysis/TMB_armor_models.R
+++ analysis/TMB_armor_models.R
@@ -6,16 +6,29 @@ chinook <- net_list[[1]] %>%
 
 
 ### First run - only continuous variables and no random effects at all ####
-data <- model.matrix(~ -1 + total + logyday + X100m, chinook) %>% as_tibble() %>% as.list()
-parameters <- list(b0=0, b1=0, b2=0, logSigma=0)
+chinook.mm <- model.matrix(~ -1 + total + logyday + X100m, chinook) %>% 
+  as_tibble() |> 
+  transform(total = c(total), #strip attributes (otherwise TMB gets mad)
+            logyday = c(logyday),
+            X100m = c(X100m))
 
-obj <- MakeADFun(data, parameters, DLL="tmb/amod")
-obj$hessian <- TRUE
+data <- list(y = chinook.mm$total,
+             logyday = chinook.mm$logyday,
+             X100m = chinook.mm$X100m)
 
-opt <- do.call("optim", obj)
-opt
+parameters <- list(b0=0, b1=0, b2=0, log_var=log(1))
 
-opt$hessian ## <-- FD hessian from optim
-obj$he()    ## <-- Analytical hessian
+compile("tmb/amod.cpp") #zero means it worked
+dyn.load(dynlib("tmb/amod"))
 
-sdreport(obj)
+model <- MakeADFun(data, parameters, DLL="amod")
+
+#Fit model
+fit <- nlminb(model$par, model$fn, model$gr)
+
+#Extract parameter estimates
+best <- model$env$last.par.best
+print(best)
+rep <- sdreport(model)
+print(summary(rep))
+
