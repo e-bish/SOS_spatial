a4c34a0c5c9c1e68c840a5c859eb76bdbd17752a
Emily Bishop <ebish@uw.edu>
2022-11-22 16:31:52 -0800

    edit R script to be independent from perc_armor_models

--- analysis/TMB_armor_models.R
+++ analysis/TMB_armor_models.R
@@ -1,27 +1,31 @@
+library(here)
+library(tidyverse)
 require(TMB)
 
-chinook <- net_list[[1]] %>% 
+#load data
+#chinook comes from perc_armor_models net_list[[1]]
+chinook<- here::here("data","chinook.csv")
+chinook <- read_csv(chinook) %>% 
   mutate(logyday = log(yday)) %>% 
   dplyr::select(-c(year, month, yday, species, rest_yr, X500m, X1.2km))
 
-
-### First run - only continuous variables and no random effects at all ####
+### start with only two continuous variables and no random effects ####
 chinook.mm <- model.matrix(~ -1 + total + logyday + X100m, chinook) %>% 
-  as_tibble() |> 
+  as_tibble() %>% 
   transform(total = c(total), #strip attributes (otherwise TMB gets mad)
             logyday = c(logyday),
             X100m = c(X100m))
 
 data <- list(y = chinook.mm$total,
-             logyday = chinook.mm$logyday,
-             X100m = chinook.mm$X100m)
+             x = chinook.mm$logyday,
+             z = chinook.mm$X100m)
 
-parameters <- list(b0=0, b1=0, b2=0, log_var=log(1))
+parameters <- list(b0=5, b1=1, b2=1, log_var=log(30)) #variance must be >mu
 
 compile("tmb/amod.cpp") #zero means it worked
 dyn.load(dynlib("tmb/amod"))
 
-model <- MakeADFun(data, parameters, DLL="amod")
+model <- MakeADFun(data, parameters, DLL="amod",hessian=T)
 
 #Fit model
 fit <- nlminb(model$par, model$fn, model$gr)
--- tmb/amod.cpp
+++ tmb/amod.cpp
@@ -5,34 +5,30 @@ Type objective_function<Type>::operator() ()
 {
   //specify data
   DATA_VECTOR(y);
-  DATA_VECTOR(logyday);
-  DATA_VECTOR(X100m);
+  DATA_VECTOR(x);
+  DATA_VECTOR(z);
   int n = y.size();
   
   //specify parameters
   PARAMETER(b0);
   PARAMETER(b1);
   PARAMETER(b2);
-  //PARAMETER(log_mu); 
   PARAMETER(log_var);
 
   //create objects
-  vector<Type> yfit(n); 
-  //Type mu=exp(log_mu);
+  vector<Type> mu(n); 
   Type var=exp(log_var);
   Type neglogL = 0.0; //initial starting value for neg log likelihood
 
   //negative log likelihood
   for(int i = 0; i < n; i++) { 
-    yfit(i) = b0 + b1 * logyday(i) + b2 * X100m(i);
-    neglogL -= sum(dnbinom2(y(i), yfit(i), var, true));
+    mu(i) = b0 + b1*x(i) + b2*z(i);
+    neglogL += -dnbinom2(y(i), mu(i), var, true);
   }
   
   //stuff to report 
   return neglogL;
   ADREPORT(var);
-  //ADREPORT(mu);
-  //ADREPORT(exp(2*logSigma));  
 }
 
 
