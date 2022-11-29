5a6aa7ae3a8964b38c3ecf8b28bd4f3dd20bc102
Emily Bishop <ebish@uw.edu>
2022-11-28 16:40:56 -0800

    clean up code and attempt random models

--- analysis/TMB_armor_models.R
+++ analysis/TMB_armor_models.R
@@ -6,9 +6,9 @@ require(TMB)
 #chinook comes from perc_armor_models net_list[[1]]
 chinook<- here::here("data","chinook.csv")
 chinook <- read_csv(chinook) %>% 
-  mutate(logyday = log(yday)) 
+  mutate(logyday = log(yday)) %>% 
+  na.omit(chinook)
 
-chinook <- na.omit(chinook)
 b0 <- 0.4
 b1 <- 1
 b2 <- -0.25
@@ -16,20 +16,17 @@ X <- model.matrix(~ scale(logyday) + scale(X100m), data = chinook)
 
 logmu <- X %*% c(b0, b1, b2)
 
+#simulate fish counts
 fake.y <- rnbinom(nrow(X), mu = exp(logmu), size = 4)
 
-### start with only two continuous variables and no random effects ####
-chinook1.mm <- model.matrix(~ -1 + total + logyday + X100m, chinook) %>% 
-  as_tibble() %>% 
-  mutate(across(everything(), as.vector)) #strip attributes (otherwise TMB gets mad)
-
-
+################################################################################
+######## start with only two continuous variables and no random effects ########
 data <- list(y = fake.y,
              X = X)
 
-#parameters <- list(b0 = 0, b1=0, b2=0, log_var=0) #variance must be >mu
-parameters <- list(beta=  rep(0, 3), 
-                   log_var=0) #variance must be >mu
+parameters <- list(beta = rep(0,3), 
+                   log_var = 0) 
+
 compile("tmb/amod.cpp") #zero means it worked
 dyn.load(dynlib("tmb/amod"))
 
@@ -44,14 +41,46 @@ print(best)
 rep <- sdreport(model)
 print(summary(rep))
 
-
 ################################################################################
-#add parameters and random effect
-chinook2.mm <- model.matrix(~ -1 + total + site + ipa + veg + rest_age + logyday + X100m, chinook) %>% 
-  as_tibble() %>% 
-  mutate(across(everything(), as.vector))
+######################### add random effect ####################################
 
+# create model matrix for the random effect (site)
 Z <- model.matrix( ~ -1 + site, data = chinook)
+
+# mean of site intercepts
 gamma <- rnorm(n = ncol(Z), mean = 0, sd = 1)
+
+#beta has to be a matrix
 beta <- matrix(c(b0, b1, b2), nrow = 3, ncol =1)
+
+#matrix algebra!
 logmu <- X %*% beta + Z %*% gamma
+
+#simulate fish counts
+fake.y <- rnbinom(nrow(X), mu = exp(logmu), size = 4)
+
+data <- list(y = fake.y,
+             X = X,
+             Z = Z)
+
+parameters <- list(beta = rep(0,3), 
+                   gamma = rep(0, ncol(Z)),
+                   log_var = 0) 
+
+compile("tmb/amod_ran.cpp") 
+dyn.load(dynlib("tmb/amod_ran"))
+
+model_ran <- MakeADFun(data, parameters, random = "gamma", DLL="amod_ran", hessian=T)
+
+#Fit model
+fit_ran <- nlminb(model_ran$par, model_ran$fn, model_ran$gr)
+
+#Extract parameter estimates
+best_ran <- model_ran$env$last.par.best
+print(best_ran)
+rep_ran <- sdreport(model_ran)
+print(summary(rep))
+
+
+
+
--- tmb/amod_ran.cpp
+++ tmb/amod_ran.cpp
@@ -6,25 +6,26 @@ Type objective_function<Type>::operator() ()
   //specify data
   DATA_VECTOR(y);
   DATA_MATRIX(X);
+  DATA_MATRIX(Z);
   int n = y.size();
   
   //specify parameters
   PARAMETER_VECTOR(beta);
+  PARAMETER_MATRIX(gamma);
   PARAMETER(log_var);
-
+  
   //create objects
   vector<Type> mu(n); 
   vector<Type> logmu(n); 
   Type var=exp(log_var);
   Type neglogL = 0.0; //initial starting value for neg log likelihood
   
-  //matrix algebra!
-  logmu = X * beta;
+  logmu = X * beta + Z * gamma;
   
   //negative log likelihood
   for(int i = 0; i < n; i++) { 
     mu(i) = exp(logmu(i));
-    neglogL -= dnbinom2(y(i), mu(i), mu(i)*(1.0+var), true);
+    neglogL -= dnbinom2(y(i), mu(i), var, true);
   }
   
   //stuff to report 
